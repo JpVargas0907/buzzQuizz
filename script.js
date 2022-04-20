7d8299c7dbbb53fa53fa1622e297d4ca6e80955f
João Paulo <joaopaulo0907@gmail.com>
2022-04-20 17:14:00 -0300

    Listagem de quizzes na tela 1 funcionando.

---
+++ script.js
@@ -0,0 +1,26 @@
+let quizzes = []
+
+buscarDados()
+
+function buscarDados(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
+
+    promise.then(carregarDados);
+}
+
+function carregarDados(response){
+    quizzes = response.data;
+    renderQuizzes();
+}
+
+function renderQuizzes(){
+    const listaQuizzes = document.querySelector(".lista-quizzes")
+
+    for(let i = 0; i <= listaQuizzes.clientHeight; i++){
+        listaQuizzes.innerHTML += `
+        <li class="quizz">
+        <img src="${quizzes[i].image}" alt="">
+        <p class="titulo-quizz">${quizzes[i].title}</p>
+        </li>`
+    }
+}
